5cc872fd3b8a06a3868cb57e8e6c9dc93f64a745
antoninogrimaldi <antoninogrimaldi@shopbest.it>
2025-12-19 14:27:18 +0100

    Load Shopify helper in offers page

--- public/offers.php
+++ public/offers.php
@@ -2,6 +2,7 @@
 require_once __DIR__ . '/../includes/auth.php';
 require_once __DIR__ . '/../includes/helpers.php';
 require_once __DIR__ . '/../includes/db.php';
+require_once __DIR__ . '/../includes/shopify.php';
 require_login();
 $user = current_user();
 $message = $error = null;
